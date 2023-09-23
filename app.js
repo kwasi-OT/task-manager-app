fa53751778fea3d45979bc54037205ba57c223cf
Kwasi O-T <tweneboah21@gmail.com>
2023-09-23 00:04:23 +0000

    updated api endpoints with that of php

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.controller('MainController', function ($scope, $http) {
 
     // Function to load tasks from API
     function loadTasks() {
-        $http.get('https://task-manager-api-kwasi-ot.vercel.app/tasks')
+        $http.get('http://your-server-url/tasks.php')
             .then(function (response) {
                 $scope.tasks = response.data;
             })
@@ -22,7 +22,7 @@ app.controller('MainController', function ($scope, $http) {
 
     // Function to add a new task
     $scope.addTask = function () {
-        $http.post('https://task-manager-api-kwasi-ot.vercel.app/tasks', $scope.newTask)
+        $http.post('http://your-server-url/tasks.php', $scope.newTask)
             .then(function () {
                 loadTasks();
                 $scope.newTask = {};
@@ -41,7 +41,7 @@ app.controller('MainController', function ($scope, $http) {
 
     // Function to save edited task
     $scope.saveTask = function () {
-        $http.put('https://task-manager-api-kwasi-ot.vercel.app/tasks/' + $scope.editTaskData.id, $scope.editTaskData)
+        $http.put('http://your-server-url/tasks.php', $scope.editTaskData)
             .then(function () {
                 loadTasks();
                 $scope.editTaskData = {};
@@ -54,7 +54,7 @@ app.controller('MainController', function ($scope, $http) {
 
     // Function to delete a task
     $scope.deleteTask = function (task) {
-        $http.delete('https://task-manager-api-kwasi-ot.vercel.app/tasks/' + task.id)
+        $http.delete('http://your-server-url/tasks.php?id=' + task.id)
             .then(function () {
                 loadTasks();
             })
@@ -66,9 +66,76 @@ app.controller('MainController', function ($scope, $http) {
     // Function to toggle task completion
     $scope.toggleCompletion = function (task) {
         task.completed = !task.completed;
-        $http.put('https://task-manager-api-kwasi-ot.vercel.app/tasks/' + task.id + '/completed', task)
+        $http.put('http://your-server-url/tasks.php', task)
             .catch(function (error) {
                 console.error('Error toggling completion:', error);
             });
     };
 });
+
+
+//     // Function to load tasks from API
+//     function loadTasks() {
+//         $http.get('https://task-manager-api-kwasi-ot.vercel.app/tasks')
+//             .then(function (response) {
+//                 $scope.tasks = response.data;
+//             })
+//             .catch(function (error) {
+//                 console.error('Error loading tasks:', error);
+//             });
+//     }
+
+//     loadTasks();
+
+//     // Function to add a new task
+//     $scope.addTask = function () {
+//         $http.post('https://task-manager-api-kwasi-ot.vercel.app/tasks', $scope.newTask)
+//             .then(function () {
+//                 loadTasks();
+//                 $scope.newTask = {};
+//             })
+//             .catch(function (error) {
+//                 console.error('Error adding task:', error);
+//             });
+//     };
+
+//     // Function to edit a task
+//     $scope.editTask = function (task) {
+//         console.log('Edit task:', task);
+//         $scope.editing = true;
+//         $scope.editTaskData = angular.copy(task);
+//     };
+
+//     // Function to save edited task
+//     $scope.saveTask = function () {
+//         $http.put('https://task-manager-api-kwasi-ot.vercel.app/tasks/' + $scope.editTaskData.id, $scope.editTaskData)
+//             .then(function () {
+//                 loadTasks();
+//                 $scope.editTaskData = {};
+//                 $scope.editing = false;
+//             })
+//             .catch(function (error) {
+//                 console.error('Error editing task:', error);
+//             });
+//     };
+
+//     // Function to delete a task
+//     $scope.deleteTask = function (task) {
+//         $http.delete('https://task-manager-api-kwasi-ot.vercel.app/tasks/' + task.id)
+//             .then(function () {
+//                 loadTasks();
+//             })
+//             .catch(function (error) {
+//                 console.error('Error deleting task:', error);
+//             });
+//     };
+
+//     // Function to toggle task completion
+//     $scope.toggleCompletion = function (task) {
+//         task.completed = !task.completed;
+//         $http.put('https://task-manager-api-kwasi-ot.vercel.app/tasks/' + task.id + '/completed', task)
+//             .catch(function (error) {
+//                 console.error('Error toggling completion:', error);
+//             });
+//     };
+// });
