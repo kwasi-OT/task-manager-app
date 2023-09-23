b3042e40192ebc2cac64fcbdb4cf6b1fab520308
Kwasi O-T <tweneboah21@gmail.com>
2023-09-23 01:34:22 +0000

    updated custom filter function

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.controller('MainController', function ($scope, $http) {
      // Custom filter function
         $scope.customFilter = function (task) {
             // If filterCompleted is true, show completed tasks; if false, show all tasks
-            return $scope.filterCompleted ? task.completed : true;
+            return $scope.filterCompleted ? !task.completed : true;
     };
 });
 
